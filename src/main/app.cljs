fa3f5134c9c16bd8289fb2eecacf420edd60d75c
Rafael Delboni <rafadelboni@gmail.com>
2023-05-30 17:51:12 -0300

    feat: pause and debug menu

--- src/main/app.cljs
+++ src/main/app.cljs
@@ -1,5 +1,6 @@
 (ns main.app
   (:require ["axios$default" :as axios]
+            ["bootstrap"]
             ["react-dom/client" :as rdom]
             [clojure.pprint :as pprint]
             [goog.object :as gobj]
@@ -45,10 +46,11 @@
     (pre-load-image (first upcoming-list))
     (pre-load-image (second upcoming-list))))
 
-(defnc countdown
+(defnc photo-switcher
   "Countdown component"
-  [{:keys [seconds]}]
+  [{:keys [seconds pause debug] :as upper-state}]
   (let [[state set-state] (hooks/use-state {:counter 0
+                                            :timeout-id nil
                                             :current nil
                                             :past {:list []}
                                             :upcoming {:error nil
@@ -63,8 +65,6 @@
       [upcoming-list upcoming-loading]
       (when (and (< (count upcoming-list) 2)
                  (not upcoming-loading))
-        #_(let [images (take 5 (repeatedly #(rand-int 1000)))]
-            (set-state update-in [:upcoming :list] #(into %2 %1) images))
         (get-30-random-portraits set-state)))
 
     (hooks/use-effect
@@ -73,11 +73,25 @@
         (set-current set-state current upcoming-list)))
 
     (hooks/use-effect
+      [pause]
+      (if pause
+        (set-state assoc :timeout-id (js/clearTimeout (:timeout-id state)))
+        (set-state assoc :timeout-id (js/setTimeout #(set-state update-in [:counter] dec) 1000))))
+
+    (hooks/use-effect
+      [seconds]
+      (do (set-state assoc :timeout-id (js/clearTimeout (:timeout-id state)))
+          (set-state assoc :counter seconds)))
+
+    (hooks/use-effect
       [counter]
-      (if-not (zero? counter)
-        (js/setTimeout #(set-state update-in [:counter] dec) 1000)
-        (do (set-state assoc :counter seconds)
-            (set-current set-state current upcoming-list))))
+      (when-not pause
+        (cond
+          ; tick on positive
+          (> counter 0) (set-state assoc :timeout-id (js/setTimeout #(set-state update-in [:counter] dec) 1000))
+          ; zero
+          :else (do (set-state assoc :counter seconds)
+                    (set-current set-state current upcoming-list)))))
 
     (d/div
      (d/div {:className "text-center"}
@@ -97,14 +111,31 @@
                       {:className "spinner-grow" :role "status"}
                       (d/span {:className "visually-hidden"} "Loading...")))))
 
-     (d/pre
-      {:className "pt-4"}
-      (d/code
-       (with-out-str (pprint/pprint state)))))))
+     (when debug
+       (d/pre
+        {:className "pt-4"}
+        (d/code
+         (with-out-str (pprint/pprint (merge state {:upper-state upper-state})))))))))
 
 ;; app
 (defnc app []
-  (let [[_state _set-state] (hooks/use-state {})]
+  (let [[state set-state] (hooks/use-state {:seconds 60
+                                            :pause false
+                                            :debug false})
+        handle-key-press (hooks/use-callback
+                           :auto-deps
+                           (fn [e]
+                             (when (= (.-keyCode e) 32) ; space key
+                               (.preventDefault e)
+                               (set-state assoc :pause (not (:pause state))))))
+
+        {:keys [seconds debug pause]} state]
+
+    (hooks/use-effect
+      [handle-key-press]
+      (.addEventListener js/document "keydown" handle-key-press)
+      (fn [] (.removeEventListener js/document "keydown" handle-key-press)))
+
     (d/div
      {:className "container"}
      (d/header
@@ -114,8 +145,53 @@
        (d/span {:className "px-2 fs-4 text-dark"} "Random Portrait"))
       (d/div
        {:className "col-md-3 text-end"}
-       (d/button {:className "btn btn-outline-dark me-2" :disabled true} "Config")))
-     ($ countdown {:seconds 10}))))
+       (d/div
+        {:className "dropdown"}
+        (d/button
+         {:className "btn btn-outline-dark me-2 dropdown-toggle"
+          :data-bs-toggle "dropdown"
+          :aria-expanded "false"
+          :data-bs-auto-close "outside"}
+         "Config")
+        (d/div
+         {:className "dropdown-menu dropdown-menu-end p-4"}
+         (d/div
+          {:className "mb-3"}
+          (d/label {:className "form-label"
+                    :for "seconds-input"} "Seconds")
+          (d/input {:className "form-control"
+                    :type "number"
+                    :id "seconds-input"
+                    :value seconds
+                    :on-change #(set-state assoc :seconds (js/Number (.. % -target -value)))}))
+         (d/div
+          {:className "mb-3"}
+          (d/div
+           {:className "form-check form-switch"}
+           (d/label {:className "form-check-label" :for "pause-switch"}
+                    (d/span {:className "pe-2"} "Pause")
+                    (d/kbd "Space"))
+           (d/input {:className "form-check-input"
+                     :type "checkbox"
+                     :id "pause-switch"
+                     :role "switch"
+                     :checked pause
+                     :on-change #(set-state assoc :pause (js/Boolean (.. % -target -checked)))})))
+         (d/div
+          {:className "mb-3"}
+          (d/div
+           {:className "form-check form-switch"}
+           (d/label {:className "form-check-label" :for "debug-switch"} "Debug")
+           (d/input {:className "form-check-input"
+                     :type "checkbox"
+                     :id "debug-switch"
+                     :role "switch"
+                     :checked debug
+                     :on-change #(set-state assoc :debug (js/Boolean (.. % -target -checked)))})))))))
+
+     ($ photo-switcher {:seconds seconds
+                        :pause pause
+                        :debug debug}))))
 
 ;; start your app with your React renderer
 (defn ^:export init []
